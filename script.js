6b25ed892b357f5ce342a8cd6c37fa173919ff86
Bernardo <bernardolr17@gmail.com>
2021-01-02 20:00:18 -0600

    Final

--- script.js
+++ script.js
@@ -34,8 +34,6 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-
-
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
